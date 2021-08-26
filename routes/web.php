e7977ca4844551225d93e8c0514d6c03c8176168
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-26 17:17:10 +0700

    Menyiapkan Fitur Autentikasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.templates.default');
+    return view('welcome');
 });
+
+Auth::routes([
+    'verify' => true
+]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
