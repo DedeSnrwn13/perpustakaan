0563ce49b290961e057826009b3ab3b0a0b89f8c
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-27 14:40:59 +0700

    Membuat User Admin

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return 'Admin Page';
+})->name('admin');
+
 Auth::routes([
     'verify' => true
 ]);
