2ea48181859d1df0aed12d4323e3b0afbda89763
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-29 15:34:55 +0700

    Membuat Fungsi Edit Data Penulis

---
+++ resources/views/admin/author/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('admin.templates.default')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Edit Data Penulis</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.author.update', $author) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group">
+                            <label for="">Nama</label>
+                            <input type="text" name="name" class="form-control" placeholder="Masukan nama penulis" value="{{ $author->name }}">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Update">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
