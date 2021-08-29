228aec3d9a348c51aca57c1d1d03fd7ff7386bea
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-29 12:23:42 +0700

    Menyingkat Generate Data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesTableSeeder::class,
-            AdminUserSeeder::class
+            AdminUserSeeder::class,
+            AuthorsTableSeeder::class,
+            BooksTableSeeder::class
         ]);
     }
 }
