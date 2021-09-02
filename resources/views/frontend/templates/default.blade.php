6cde0bd7708b420d3c9cce05b75620eb12bd5363
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-09-02 13:17:07 +0700

    Membuat Template Halaman Depan

---
+++ resources/views/frontend/templates/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('frontend.templates.partials.head')
+<body>
+    <!-- Navbar  -->
+    @include('frontend.templates.partials.navbar')
+
+    <!-- Content -->
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- Compiled and minified JavaScript -->
+    @include('frontend.templates.partials.scripts')
+</body>
+</html>
